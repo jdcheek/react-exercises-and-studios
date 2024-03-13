ee52f3ecab780aba6649fd732478c4633ce95123
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-03-12 20:45:59 -0500

    chapter 30 exercises

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -2,11 +2,15 @@ import './styles.css';
 
 function Button() {
 
-   return ( 
-         <button>
-            Learn More
-         </button>
-   );
+  function onLearnMore() {
+    alert('Splish Splash')
+  }
+
+  return (
+    <button onClick={onLearnMore}>
+      Learn More
+    </button>
+  );
 }
 
 export default Button;
